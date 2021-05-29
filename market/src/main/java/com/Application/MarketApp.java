2c938a05a5186f9a6dde1c4d07a9244dffabe5b8
Vango199 <62698943+Vango199@users.noreply.github.com>
2021-05-29 09:50:22 +0200

    petites modif de path

--- market/src/main/java/com/Application/MarketApp.java
+++ market/src/main/java/com/Application/MarketApp.java
@@ -1,4 +1,6 @@
-package Application;
+package com.Application;
+
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- market/src/main/java/com/Controllers/MarketController.java
+++ market/src/main/java/com/Controllers/MarketController.java
@@ -1,17 +1,19 @@
-package Controllers;
+package com.Controllers;
 
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 import DTOs.CardDto;
 import DTOs.UserDto;
